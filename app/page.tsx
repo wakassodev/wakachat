5a1448206448701ff0ff1a6bb34fc313f227f495
wakassodev <wakasso94@gmail.com>
2024-10-25 21:47:59 +0400

    Initialize Convex with messages API

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,37 @@
+"use client";
+
+import { useState } from "react";
+import { useMutation, useQuery } from 'convex/react';
+import { api } from "../convex/_generated/api";
+
 export default function Home() {
-  return <div>Hello World</div>;
+  const messages = useQuery(api.functions.message.list);
+  const createMessage = useMutation(api.functions.message.create);
+  const [input, setInput] = useState("");
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    createMessage({ sender: "Wakasso", content: input });
+    setInput("");
+  };
+
+  return (
+    <div>
+      {messages?.map((message, index) => (
+        <div key={index}>
+          <strong>{message.sender}:</strong> {message.content}
+        </div>
+      ))}
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="message"
+          id="message"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button type="submit">Send</button>
+      </form>
+    </div>
+  );
 }
